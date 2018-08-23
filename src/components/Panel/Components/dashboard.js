126b7e0caa026ba2d5345899f5c46fd81950a522
anshuman-cedcoss <anshumansingh@cedcoss.com>
2018-08-23 11:50:24 +0530

    Dashboard

--- src/components/Panel/Components/dashboard.js
+++ src/components/Panel/Components/dashboard.js
@@ -7,7 +7,7 @@ import { Page,
     Label,
     Checkbox, Tooltip, Link, Icon } from '@shopify/polaris';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCheck, faCheckCircle} from '@fortawesome/free-solid-svg-icons';
+import {faExclamation, faCheckCircle} from '@fortawesome/free-solid-svg-icons';
 
 class Dashboard extends Component {
     constructor(props) {
@@ -69,8 +69,12 @@ class Dashboard extends Component {
                         <Card key={keys} title={`Step ${parseInt(keys) +1}:`} subdued={this.state.stepData[keys].optional}>
                             <div className="pb-5 pl-5 pr-5 pt-5 pt-sm-1">
                                 <div className="row">
-                                    <div className="col-sm-10 col-7 pt-sm-5"><h4>{this.state.stepData[keys].message} {this.state.stepData[keys].link}</h4></div>
-                                    <div className="col-sm-2 col-3 pt-sm-3"><FontAwesomeIcon icon={faCheckCircle} size="5x" color="#0f0"/></div>
+                                    <div className="col-sm-10 col-7 pt-sm-5">
+                                        <h4>{this.state.stepData[keys].message} {this.state.stepData[keys].link} {!this.state.stepData[keys].optional?'':'(Optional)'}</h4>
+                                    </div>
+                                    <div className="col-sm-2 col-3 pt-sm-3">
+                                        <FontAwesomeIcon icon={!this.state.stepData[keys].optional?faCheckCircle:faExclamation} size="5x" color={!this.state.stepData[keys].optional?'#0f0':'#FFFF66'}/>
+                                    </div>
                                 </div>
                             </div>
                         </Card>
