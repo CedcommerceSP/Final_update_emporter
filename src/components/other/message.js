48c01b2d2e5cad90c53c8e008203694081d5b549
pradeep-cedcoss <pradeepsingh@cedcoss.com>
2018-08-28 20:23:17 +0530

    improved show message

--- src/components/other/message.js
+++ src/components/other/message.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import { Page, Card } from '@shopify/polaris';
+import { Page,EmptyState } from '@shopify/polaris';
 import * as queryString  from 'query-string';
+import "./message.css"
+import {Col,Card,Row,CardHeader,CardBody} from "reactstrap";
 
 class MessageShow extends Component {
     constructor(props) {
@@ -13,37 +15,43 @@ class MessageShow extends Component {
     }
     componentWillMount() {
        const queryParams = queryString.parse(this.props.location.search);
-       if ( queryParams.success === undefined || queryParams.success === 'false') {
-           queryParams.success = 'bg-danger';
-       } else {
-           queryParams.success = 'bg-success';
+       if(queryParams.message!=null && queryParams.success!=null) {
+           if (queryParams.success === undefined || queryParams.success === 'false') {
+               queryParams.success = 'bg-danger';
+           } else {
+               queryParams.success = 'bg-success';
+           }
+           this.setState({
+               message: queryParams.message,
+               title: queryParams.title,
+               success: queryParams.success,
+           });
        }
-       this.setState({
-           message: queryParams.message,
-           title: queryParams.title,
-           success: queryParams.success,
-       });
+       else
+       {
+           this.redirect('/auth');
+       }
+    }
+    redirect(url) {
+        this.props.history.push(url);
     }
     render() {
         return (
-            <Page title="">
-                <div className="row" style={{margin: '20% 0% 0%'}}>
-                    <div className="col-12 col-sm-8 offset-0 offset-sm-2">
-                        <div className="d-flex justify-content-center">
-                            <div className="card p-4" style={{width: '100%','minHeight':'200px'}}>
-                                <div className={`card ${this.state.success} text-center`} style={{width: '100%',minHeight:'200px'}}>
-                                    <div className="card-header">
-                                        <h1>{this.state.title}</h1>
-                                    </div>
+                <div className="row text-center">
+                    <div className="col-12 col-sm-8 col-md-8 offset-0 offset-sm-2 offset-md-2">
+                            <div className={`card p-4`} style={{width: '100%','minHeight':'200px'}}>
+                                <div className={`card ${this.state.success} text-center text-white`} style={{width: '100%',minHeight:'400px'}}>
                                     <div className="card-body">
-                                        <h1>{this.state.message}</h1>
+                                        <div className="row mt-5">
+                                            <div className="col-md-12">
+                                            <h1>{this.state.message}</h1>
+                                            </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                            </div>
                     </div>
                 </div>
-            </Page>
         );
     }
 }
