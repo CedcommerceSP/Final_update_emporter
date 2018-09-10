575ad754f441104668847f63be1bff5a1fa29cd4
anshuman-cedcoss <anshumansingh@cedcoss.com>
2018-09-10 14:21:39 +0530

    Import/Upload Steps

--- src/components/Panel/Components/import.js
+++ src/components/Panel/Components/import.js
@@ -569,7 +569,7 @@ export class Import extends Component {
                             <div className="modal-body" >
                                 <div className="row">
                                     <div className="col-12">
-                                        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                                        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel" data-interval="4000">
                                             <ol className="carousel-indicators">
                                                 <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"/>
                                                 <li data-target="#carouselExampleIndicators" data-slide-to="1"/>
@@ -584,49 +584,49 @@ export class Import extends Component {
                                                     <img className="d-block w-100" src={require("../../../assets/img/step_1.jpg")} alt="First slide" height={480}/>
                                                     <div className="carousel-caption text-dark">
                                                         <h2>Step 1</h2>
-                                                        <h3 className="bg-dark text-white">Choose Import To Start The Import Process</h3>
+                                                        <h3>Choose Import To Start The Import Process</h3>
                                                     </div>
                                                 </div>
                                                 <div className="carousel-item">
                                                     <img className="d-block w-100" src={require("../../../assets/img/step_2.1.jpg")} alt="Second slide" height={480}/>
                                                     <div className="carousel-caption d-none d-md-block text-dark">
                                                         <h2>Step 2</h2>
-                                                        <h3>Follow The Step</h3>
+                                                        <h3>Choose The Source Destination.</h3>
                                                     </div>
                                                 </div>
                                                 <div className="carousel-item">
                                                     <img className="d-block w-100" src={require("../../../assets/img/step_2.2.jpg")} alt="Third slide" height={480}/>
                                                     <div className="carousel-caption d-none d-md-block text-dark">
                                                         <h2>Step 3</h2>
-                                                        <h3>Follow The Step</h3>
+                                                        <h3>Now Import Product From Source Destination</h3>
                                                     </div>
                                                 </div>
                                                 <div className="carousel-item">
                                                     <img className="d-block w-100" src={require("../../../assets/img/step_2.5.jpg")} alt="Third slide" height={480}/>
                                                     <div className="carousel-caption d-none d-md-block text-dark">
                                                         <h2>Step 4</h2>
-                                                        <h3>Follow The Step</h3>
+                                                        <h3>In this page you can see the progress of your operations</h3>
                                                     </div>
                                                 </div>
                                                 <div className="carousel-item">
                                                     <img className="d-block w-100" src={require("../../../assets/img/step_3.1.jpg")} alt="Third slide" height={480}/>
                                                     <div className="carousel-caption d-none d-md-block text-dark">
                                                         <h2>Step 5</h2>
-                                                        <h3>Follow The Step</h3>
+                                                        <h3>All Completed Task Are Shown Here</h3>
                                                     </div>
                                                 </div>
                                                 <div className="carousel-item">
                                                     <img className="d-block w-100" src={require("../../../assets/img/step_4.1.jpg")} alt="Third slide" height={480}/>
                                                     <div className="carousel-caption d-none d-md-block text-dark">
                                                         <h2>Step 6</h2>
-                                                        <h3>Follow The Step</h3>
+                                                        <h3>Now You can Upload Products</h3>
                                                     </div>
                                                 </div>
                                                 <div className="carousel-item">
                                                     <img className="d-block w-100" src={require("../../../assets/img/step_4.2.jpg")} alt="Third slide" height={480}/>
                                                     <div className="carousel-caption d-none d-md-block text-dark">
                                                         <h2>Step 7</h2>
-                                                        <h3>Follow The Step</h3>
+                                                        <h3>Fill The Form And Click Upload To start The Process</h3>
                                                     </div>
                                                 </div>
                                             </div>
@@ -649,7 +649,7 @@ export class Import extends Component {
                                     {/*<h2>Step 1</h2>*/}
                                     {/*<h3>Choose Import To Start The Import Process</h3>*/}
                                 {/*</div>*/}
-                            {/*</div>*/}
+                            { /*</div>*/}
                         </div>
                     </div>
                 </div>
